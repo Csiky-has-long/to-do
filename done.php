3bedcdeef542d5592e3f51553209b601912d76e3
Kovács Tibor <kovacs.tibor@diak.szbi-pg.hu>
2026-03-05 08:41:44 +0100

    hatha

--- done.php
+++ done.php
@@ -1,10 +1,13 @@
 <?php
+
+$id = intval($_POST['id']);
+    $value = $_POST['value'];
+
 $conn = new mysqli('localhost', 'root', 'mysql', 'to_do');
 
 if (isset($_POST['id']) && isset($_POST['value'])) {
 
-    $id = intval($_POST['id']);
-    $value = $_POST['value'];
+
 
     if ($value === "☐" || $value === "☑") {
 
